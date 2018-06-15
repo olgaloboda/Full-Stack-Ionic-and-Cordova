7a0196461297c7a1f9e2ea89522a802ff0281990
Olga Loboda <olga.loboda.13@gmail.com>
2018-06-15 21:52:40 +0100

    Forms

--- src/pages/reservation/reservation.ts
+++ src/pages/reservation/reservation.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 /**
  * Generated class for the ReservationPage page.
  *
@@ -15,11 +16,30 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ReservationPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+	reservation: FormGroup;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ReservationPage');
-  }
+	constructor(public navCtrl: NavController, 
+				  public navParams: NavParams,
+				  public viewCtrl: ViewController,
+				  private formBuilder: FormBuilder) {
+		this.reservation = this.formBuilder.group({
+			guests: 3,
+			smoking: false,
+			dateTime: ['', Validators.required]
+		});
+	}
+
+	ionViewDidLoad() {
+	console.log('ionViewDidLoad ReservationPage');
+	}
+
+	dismiss() {
+		this.viewCtrl.dismiss();
+	}
+
+	onSubmit() {
+		console.log(this.reservation.value);
+		this.viewCtrl.dismiss();
+	}
 
 }
